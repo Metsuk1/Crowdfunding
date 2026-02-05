1a64d1f0f3bceda2b46b8393d4f7479bfa2869a5
ArsikIT <arslan.zheksenbayev@mail.ru>
2026-02-05 21:24:29 +0500

    Creating new contract + Token

---
+++ backend/contracts/CrowdToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// Импорт стандарта ERC-20
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RewardToken is ERC20 {
+    address public minter;
+
+    constructor() ERC20("Crowdfund Reward", "CFRE") {
+        minter = msg.sender;
+    }
+
+    // Функция минтинга токенов за вклады [cite: 45, 49]
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == minter, "Only contract can mint");
+        _mint(to, amount);
+    }
+}
